72ab7191071692865df42e6ce46cf8a290f848eb
izooh <isaacopicho@gmail.com>
2019-02-21 16:13:19 +0300

    first

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 
+
 Vue.use(Router)
 
 export default new Router({
@@ -13,6 +14,8 @@ export default new Router({
       name: 'home',
       component: Home
     },
+
+
     {
       path: '/about',
       name: 'about',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+<div class="dashboard">
+<h1 class="subheading grey--text">Dashboard</h1>
+<v-container>
+<v-card flat class='pa-3' v-for="project in projects" :key="project.title">
+<v-layout row wrap :class="`project ${project.status}`">
+<v-flex xs12 md6>
+<div class='caption grey--text'>project title</div>
+<div>{{project.title}}</div>
+</v-flex>
+<v-flex xs6 md2>
+<div class='caption grey--text'>person</div>
+<div>{{project.person}}a</div>
+</v-flex>
+<v-flex xs6 md2>
+<div class='caption grey--text'>Due by</div>
+<div>{{project.due}}</div>
+</v-flex>
+<v-flex xs6 md2>
+<div class='caption grey--text'>Status</div>
+<div>{{project.status}}</div>
+</v-flex>
+</v-layout>
+<v-divider></v-divider>
+</v-card>
+</v-container>
+</div>
+</div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+export default{
+data(){
+return{
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+projects:[
+{title:'interior design',person:'opicho',due:'1st jan 2019',status:'ongoing'},
+{title:'software architect',person:'ngoroboi',due:'1st jan 2018',status:'pending'},
+{title:'aero space engineer',person:'Ann',due:'3rd jan 2019',status:'ongoing'}
+]
+
+}
+}
 }
 </script>
+<style>
+.project.ongoing{
+border-left:4px solid #3cd1c2;
+}
+.project.pending{
+border-left:4px solid red;
+}
+</style>
